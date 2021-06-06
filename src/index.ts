3bf02d599d2138eb0cc529fb90fd9a770ed8c947
xunder-matth <xundersa@gmail.com>
2021-06-06 19:37:03 +0200

    Enabled banner

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ import * as ServerInfo from "./modules/ServerInfo";
 import * as Statistics from "./modules/Statistics";
 import * as CommunityServers from "./modules/CommunityServers";
 import * as Welcomer from "./modules/Welcomer";
-// import * as Banner from "./modules/Banner";
+import * as Banner from "./modules/Banner";
 import * as Staff from "./modules/Staff";
-// import path from "path";
-// import http from "http";
-// import fs from "fs";
+import path from "path";
+import http from "http";
+import fs from "fs";
 
 const main = async () => {
     console.log(`Trying to connect to ${process.env.TS3_HOST}:${process.env.TS3_PORT}`);
@@ -25,7 +25,7 @@ const main = async () => {
         ServerInfo.updateServerInfo();
         CommunityServers.updateCommunityServers();
         Welcomer.registerWelcomer();
-        // Banner.Generate();
+        Banner.Generate();
         Staff.Update();
 
         RankConnection.connect(error => {
@@ -39,45 +39,20 @@ const main = async () => {
             Statistics.updateServerStatistics();
         });
 
-        // http.createServer((req, res) => {
-        //     if (req.method === "GET" && req.url === "/banner") {
-        //         res.writeHead(200, { 'Content-Type': 'image/png' });
-        //         fs.createReadStream(path.join(__dirname + '/resources/banner.png')).pipe(res);
-        //     } else {
-        //         res.setHeader('Content-Type', 'text/plain');
-        //         res.statusCode = 404;
-        //         res.end("Not found!");
-        //     }
-        // }).listen(PORT, () => {
-        //     console.log(`HTTP Listening to port ${PORT}.`);
-        // });
-
-        // const channelList = await TeamSpeakConnection.channelList();
-
-        // let foundFirstDelimiter = false,
-        //     foundSecondDelimiter = false;
-
-        // for (const channel of channelList) {
-        //     if (channel.cid === '75') {
-        //         foundFirstDelimiter = true;
-        //         continue;
-        //     } else if (channel.cid === '77') {
-        //         foundFirstDelimiter = true;
-        //         break;
-        //     }
-
-        //     if (foundFirstDelimiter && !foundSecondDelimiter) {
-        //         console.log(`${channel.cid} > ${channel.name}`);
-        //     }
-        // }
-
-        // // channelList.sort((a, b): number => {
-        // //     return +a.order - +b.order;
-        // // }).forEach((channel) => {
-        // //     if (channel.pid === '0') {
-        // //         console.log(`${channel.order} -> ${channel.name}`);
-        // //     }
-        // // });
+        if (PORT) {
+            http.createServer((req, res) => {
+                if (req.method === "GET" && req.url === "/banner") {
+                    res.writeHead(200, { 'Content-Type': 'image/png' });
+                    fs.createReadStream(path.join(__dirname + '/resources/banner.png')).pipe(res);
+                } else {
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.statusCode = 404;
+                    res.end("Not found!");
+                }
+            }).listen(PORT, () => {
+                console.log(`HTTP Listening to port ${PORT}.`);
+            });
+        }
 
         teamspeak.on("error", (e) => {
             console.log("An error occured", e);
