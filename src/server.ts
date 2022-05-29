0d4f668f46a11f0604d80f14adfaea7b46311932
xunder-matth <xundersa@gmail.com>
2022-05-29 18:49:22 +0200

    Added `mtasa` community server ping

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import {
-    update_interval as UPDATE_INTERVAL,
-    servers,
-    channel_name,
+	update_interval as UPDATE_INTERVAL,
+	servers,
+	channel_name,
 } from "./config/servers.json";
 import { Type, query as gamedigQuery } from "gamedig";
 import { Terminal } from "./terminal";
@@ -10,117 +10,132 @@ import TSConnection from "./connection";
 const terminal = new Terminal();
 
 export const registerCommunityServers = async (): Promise<void> => {
-    for (const server of servers) {
-        const channel = await TSConnection.getChannelById(
-            server.channelid.toString()
-        );
+	for (const server of servers) {
+		const channel = await TSConnection.getChannelById(
+			server.channelid.toString()
+		);
 
-        if (!channel) {
-            terminal.redLog(
-                `Channel ${server.channelid} for server info does not exits.`
-            );
-            continue;
-        }
+		if (!channel) {
+			terminal.redLog(
+				`Channel ${server.channelid} for server info does not exits.`
+			);
+			continue;
+		}
 
-        let connectedPlayers = 0;
-        let maxPlayers = 0;
+		let connectedPlayers = 0;
+		let maxPlayers = 0;
 
-        try {
-            const result = await gamedigQuery({
-                host: server.ip,
-                port: server.port,
-                type: server.type as Type,
-            });
+		try {
+			const result = await gamedigQuery({
+				host: server.ip,
+				port: server.port,
+				type: server.type as Type,
+			});
 
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const raw: any = result.raw;
-            let desription = "";
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const raw: any = result.raw;
+			let desription = "";
 
-            switch (server.type) {
-                case "samp": {
-                    if (raw && "numplayers" in raw) {
-                        connectedPlayers = raw["numplayers"];
-                    }
+			switch (server.type) {
+				case "mtasa": {
+					if (raw && "numplayers" in raw) {
+						connectedPlayers = raw["numplayers"];
+					}
 
-                    maxPlayers = result.maxplayers;
+					maxPlayers = result.maxplayers;
 
-                    desription =
-                        `[size=9]Hostname: [COLOR=#990000][B]${result.name}[/B][/COLOR]` +
-                        "\n" +
-                        `Address: [COLOR=#990000][B]${server.ip}:${server.port}[/B][/COLOR]` +
-                        "\n" +
-                        `Players: [COLOR=#990000][B]${connectedPlayers}/${maxPlayers}[/B][/COLOR][/size]`;
-                    break;
-                }
-                case "cs16": {
-                    if (raw && "numplayers" in raw) {
-                        connectedPlayers = raw["numplayers"];
-                    }
+					desription =
+						`[size=9]Hostname: [COLOR=#990000][B]${result.name}[/B][/COLOR]` +
+						"\n" +
+						`Address: [COLOR=#990000][B]${server.ip}:${server.port}[/B][/COLOR]` +
+						"\n" +
+						`Players: [COLOR=#990000][B]${connectedPlayers}/${maxPlayers}[/B][/COLOR][/size]`;
+					break;
+				}
+				case "samp": {
+					if (raw && "numplayers" in raw) {
+						connectedPlayers = raw["numplayers"];
+					}
 
-                    maxPlayers = result.maxplayers;
+					maxPlayers = result.maxplayers;
 
-                    desription =
-                        `[size=9]Hostname: [COLOR=#990000][B]${result.name}[/B][/COLOR]` +
-                        "\n" +
-                        `Address: [COLOR=#990000][B]${server.ip}:${server.port}[/B][/COLOR]` +
-                        "\n" +
-                        `Players: [COLOR=#990000][B]${connectedPlayers}/${maxPlayers}[/B][/COLOR][/size]`;
-                    break;
-                }
-                case "minecraft": {
-                    connectedPlayers = result.players.length;
-                    maxPlayers = result.maxplayers;
+					desription =
+						`[size=9]Hostname: [COLOR=#990000][B]${result.name}[/B][/COLOR]` +
+						"\n" +
+						`Address: [COLOR=#990000][B]${server.ip}:${server.port}[/B][/COLOR]` +
+						"\n" +
+						`Players: [COLOR=#990000][B]${connectedPlayers}/${maxPlayers}[/B][/COLOR][/size]`;
+					break;
+				}
+				case "cs16": {
+					if (raw && "numplayers" in raw) {
+						connectedPlayers = raw["numplayers"];
+					}
 
-                    desription =
-                        `[size=9]Hostname: [COLOR=#990000][B]${result.name}[/B][/COLOR]` +
-                        "\n" +
-                        `Address: [COLOR=#990000][B]${server.hub_ip}[/B][/COLOR]` +
-                        "\n" +
-                        `Players: [COLOR=#990000][B]${connectedPlayers}/${maxPlayers}[/B][/COLOR][/size]`;
-                    break;
-                }
-                default:
-                    break;
-            }
+					maxPlayers = result.maxplayers;
 
-            const newName = channel_name
-                .replace("%gametext%", server.gametext)
-                .replace("%gamemode%", server.gamemode)
-                .replace("%onlineplayers%", connectedPlayers.toString())
-                .replace("%maxplayers%", maxPlayers.toString());
+					desription =
+						`[size=9]Hostname: [COLOR=#990000][B]${result.name}[/B][/COLOR]` +
+						"\n" +
+						`Address: [COLOR=#990000][B]${server.ip}:${server.port}[/B][/COLOR]` +
+						"\n" +
+						`Players: [COLOR=#990000][B]${connectedPlayers}/${maxPlayers}[/B][/COLOR][/size]`;
+					break;
+				}
+				// case "minecraft": {
+				// 	connectedPlayers = result.players.length;
+				// 	maxPlayers = result.maxplayers;
 
-            if (channel.name === newName) {
-                continue;
-            }
+				// 	desription =
+				// 		`[size=9]Hostname: [COLOR=#990000][B]${result.name}[/B][/COLOR]` +
+				// 		"\n" +
+				// 		`Address: [COLOR=#990000][B]${server.hub_ip}[/B][/COLOR]` +
+				// 		"\n" +
+				// 		`Players: [COLOR=#990000][B]${connectedPlayers}/${maxPlayers}[/B][/COLOR][/size]`;
+				// 	break;
+				// }
+				default:
+					break;
+			}
 
-            channel.edit({
-                channelName: newName,
-                channelDescription:
-                    desription +
-                    "\n" +
-                    "[center][B][size=8][hr]» [COLOR=#990000]Skill Arena[/COLOR] Public TeamSpeak «" +
-                    "\n" +
-                    "» Contact: [COLOR=#990000]public@sa-rpg.com[/COLOR] «[hr][/size][/B][/center]",
-            });
-        } catch (error) {
-            const newName = `[cspacer]${server.gametext} • ${server.gamemode} • ${connectedPlayers}/${maxPlayers}`;
+			const newName = channel_name
+				.replace("%gametext%", server.gametext)
+				.replace("%gamemode%", server.gamemode)
+				.replace("%onlineplayers%", connectedPlayers.toString())
+				.replace("%maxplayers%", maxPlayers.toString());
 
-            if (channel.name === newName) {
-                continue;
-            }
+			if (channel.name === newName) {
+				continue;
+			}
 
-            channel.edit({
-                channelName: newName,
-                channelDescription:
-                    "[center][B][size=8][hr]» [COLOR=#990000]Skill Arena[/COLOR] Public TeamSpeak «" +
-                    "\n" +
-                    "» Contact: [COLOR=#990000]public@sa-rpg.com[/COLOR] «[hr][/size][/B][/center]",
-            });
+			channel.edit({
+				channelName: newName,
+				channelDescription:
+					desription +
+					"\n" +
+					"[center][B][size=8][hr]» [COLOR=#990000]Skill Arena[/COLOR] Public TeamSpeak «" +
+					"\n" +
+					"» Contact: [COLOR=#990000]public@sa-rpg.com[/COLOR] «[hr][/size][/B][/center]",
+			});
+		} catch (error) {
+			const newName = `[cspacer]${server.gametext} • ${server.gamemode} • ${connectedPlayers}/${maxPlayers}`;
 
-            terminal.redLog(
-                `Error querying server, game: ${server.gametext}, game: ${server.gamemode}, (${server.ip}:${server.port})`
-            );
-        }
-    }
-    setTimeout(registerCommunityServers, UPDATE_INTERVAL * 1000);
+			if (channel.name === newName) {
+				continue;
+			}
+
+			channel.edit({
+				channelName: newName,
+				channelDescription:
+					"[center][B][size=8][hr]» [COLOR=#990000]Skill Arena[/COLOR] Public TeamSpeak «" +
+					"\n" +
+					"» Contact: [COLOR=#990000]public@sa-rpg.com[/COLOR] «[hr][/size][/B][/center]",
+			});
+
+			terminal.redLog(
+				`Error querying server, game: ${server.gametext}, game: ${server.gamemode}, (${server.ip}:${server.port})`
+			);
+		}
+	}
+	setTimeout(registerCommunityServers, UPDATE_INTERVAL * 1000);
 };
